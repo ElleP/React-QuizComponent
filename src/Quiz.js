c6185ab900232614d5d48361b842a8d4eaf5ddc3
Michelle Pantorp <michelle.pantorp@gmail.com>
2018-06-14 09:57:12 +0200

    fixed error for quizquestion component

--- src/Quiz.js
+++ src/Quiz.js
@@ -1,7 +1,7 @@
-import React, { Component } from 'react';
-import QuizQuestion from './QuizQuestion.js';
+import React, { Component } from 'react'
+import QuizQuestion from './QuizQuestion.js'
 
-let quizData = require('./quiz_data.json');
+let quizData = require('./quiz_data.json')
 
 class Quiz extends Component {
   constructor(props) {
@@ -13,7 +13,7 @@ class Quiz extends Component {
   render() {
     return(
       <div>
-        <QuizQuestion quiz_question={quizData.quiz_question[this.state.quiz_position - 1]}/>
+        <QuizQuestion quiz_question={quizData.quiz_questions[this.state.quiz_position - 1 ]}/>
       </div>
     )
   }
